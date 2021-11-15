83406097e6b83e2bc95ce7077f70463ad196da3e
peraba <93363482+peraba@users.noreply.github.com>
2021-11-14 18:07:10 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "1px";
+dodger.style.left = "1px";
+moveDodgerRight()
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
